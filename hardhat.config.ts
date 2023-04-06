c3a49163665e65519b27b4b33b33bb58d37f8ec7
Bobbie946 <bobzxc50@gmail.com>
2023-04-06 05:29:19 +0000

    Standerdize solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -52,7 +52,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
+        version: "0.8.9",
         settings: {
           metadata: {
             bytecodeHash: "ipfs",
